82c26f286f7cf80deeab8f714212d9dc66733f39
gtguizhou <gtguizhou@qq.com>
2019-03-27 17:20:56 +0800

    更新

--- application/common/ext/RedisPool.php
+++ application/common/ext/RedisPool.php
@@ -21,7 +21,7 @@ class RedisPool
             self::$redis = new \Redis();
             if (!self::$redis)
                 throw_validate_exception('redis服务初始化失败');
-            self::$redis->connect(config('redis.hostname'),config('redis.port'));
+            self::$redis->connect(config('redis.hostname','127.0.0.1'),config('redis.port','6379'));
         }
 
         return self::$redis;
